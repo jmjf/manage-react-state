6882da1baae06b1375e84442bb0f877d7b6ac15d
jmjf <jamee.mikell@gmail.com>
2022-06-26 19:12:58 +0000

    2.0.2 - FEAT: show shoes available and allow customer to filter list by size

--- notes/02-LocalRemoteState.md
+++ notes/02-LocalRemoteState.md
@@ -30,3 +30,61 @@ See `README.md` for info on how he changed from `create-react-app` default.
 **COMMIT 2.0.1 - CHORE: setup base environment from instructor's starter with my changes**
 
 ## useState and hooks
+
+Build the shoe listing page.
+
+-  Copy shoe (products) array from `db.json` into `App` (using local data for now)
+-  Display the products
+   -  New `<section>` (more semantically accurate that `<div>`)
+   -  Use `map()` to get `renderProduct()` result for each product
+   -  Uncomment `renderProduct`
+-  Should get shoes on app now
+
+Looking at the data, we have sizes for the shoes (per SKU). Customers will want to filter for shoes available in their size.
+
+-  We have a size filter `<select>` on the page
+-  Need to get it's value and filter results (probably `products.filter()`) based on it
+-  `useState()` seems like a practical solution given
+   -  This is in `App` (top level) instead of a component (like it should be)
+   -  We only have one value to track (size selected)
+   -  We only need it in this component
+-  So, my solution is to `useState()` for the size, bind it to the `<select>`'s value, and filter products based on that value
+   -  In the products `<section>`, I used `some()` to find products where at least one sku matches the selected size (or all shoes if selected size is empty string)
+   -  I chained `products.filter(product.skus.some()).map()`
+   -  And it works
+-  Notes after watching instructor solve it
+   -  Uses different names, but I'd argue mine are more explicit about intent
+   -  On the `<select>`, in addition to writing `onChange`, he sets `value={selectedSize}`
+      -  Not sure `value` offers any value over the event handler
+   -  He filters products in a `const` before the return, which is reasonable--I was thinking the filter needed to be cleaned up a bit to declutter the JSX (applied this change)
+
+### Hooks overview
+
+-  Hooks are for function components
+   -  Using in classes requires different technique
+-  Start with "use" (by convention)
+-  Call at the top level of component, not inside nested function, conditionals, loops
+   -  No `if (...) { const [s, setS] = useState(''); }`
+   -  No `function Comp1 { let s = false; let setS = () => {}; function nested() { [s, setS] = useState(false); } }`
+   -  Best practice is to put hooks at the top of the component
+
+### React dev tools
+
+-  You can see state values in RDT
+   -  Doesn't know what they're called, so you need to know the order they were invoked
+
+### When does it render
+
+-  State changes
+-  Prop changes
+-  Parent component renders
+-  Context changes
+-  Variables must be derived/calculated based on one of the above to change
+
+Suppress renders (performance) with
+
+-  State change -> `shouldComponentUpdate` or `Memo`
+-  Prop/parent change -> same plus `PureComponent`
+-  Context -> none
+
+**COMMIT: 2.0.2 - FEAT: show shoes available and allow customer to filter list by size**
--- shoe-store/src/App.tsx
+++ shoe-store/src/App.tsx
@@ -1,38 +1,108 @@
-import React from "react";
-import "./App.css";
-import Footer from "./Footer";
-import Header from "./Header";
+import React, { useState } from 'react';
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+
+interface ISku {
+	sku: string;
+	size: number;
+}
+interface IProduct {
+	id: number;
+	category: string;
+	image: string;
+	name: string;
+	price: number;
+	skus: ISku[];
+	description: string;
+}
+
+const products: IProduct[] = [
+	{
+		id: 1,
+		category: 'shoes',
+		image: 'shoe1.jpg',
+		name: 'Hiker',
+		price: 94.95,
+		skus: [
+			{ sku: '17', size: 7 },
+			{ sku: '18', size: 8 },
+		],
+		description: 'This rugged boot will get you up the mountain safely.',
+	},
+	{
+		id: 2,
+		category: 'shoes',
+		image: 'shoe2.jpg',
+		name: 'Climber',
+		price: 78.99,
+		skus: [
+			{ sku: '28', size: 8 },
+			{ sku: '29', size: 9 },
+		],
+		description: 'Sure-footed traction in slippery conditions.',
+	},
+	{
+		id: 3,
+		category: 'shoes',
+		image: 'shoe3.jpg',
+		name: 'Explorer',
+		price: 145.95,
+		skus: [
+			{ sku: '37', size: 7 },
+			{ sku: '38', size: 8 },
+			{ sku: '39', size: 9 },
+		],
+		description: 'Look stylish while stomping in the mud.',
+	},
+];
 
 export default function App() {
-  // function renderProduct(p) {
-  //   return (
-  //     <div key={p.id} className="product">
-  //       <a href="/">
-  //         <img src={`/images/${p.image}`} alt={p.name} />
-  //         <h3>{p.name}</h3>
-  //         <p>${p.price}</p>
-  //       </a>
-  //     </div>
-  //   );
-  // }
+	const [selectedSize, setSelectedSize] = useState('');
 
-  return (
-    <>
-      <div className="content">
-        <Header />
-        <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>{" "}
-            <select id="size">
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-          </section>
-        </main>
-      </div>
-      <Footer />
-    </>
-  );
+	function renderProduct(p: IProduct) {
+		return (
+			<div key={p.id} className="product">
+				<a href="/">
+					<img src={`/images/${p.image}`} alt={p.name} />
+					<h3>{p.name}</h3>
+					<p>${p.price}</p>
+				</a>
+			</div>
+		);
+	}
+
+	const filteredProducts =
+		selectedSize !== ''
+			? products.filter((product) =>
+					product.skus.some((sku) => selectedSize === '' || sku.size.toString() === selectedSize)
+			  )
+			: products;
+
+	return (
+		<>
+			<div className="content">
+				<Header />
+				<main>
+					<section id="filters">
+						<label htmlFor="size">Filter by Size:</label>{' '}
+						<select
+							id="size"
+							value={selectedSize}
+							onChange={(e) => {
+								setSelectedSize(e.target.value);
+							}}
+						>
+							<option value="">All sizes</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+							<option value="9">9</option>
+						</select>
+					</section>
+					<section id="products">{filteredProducts.map(renderProduct)}</section>
+				</main>
+			</div>
+			<Footer />
+		</>
+	);
 }
