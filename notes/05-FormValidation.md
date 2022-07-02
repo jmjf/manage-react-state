d793cfcfd73c8d29f25da49f313c63e61891d8d5
jmjf <jamee.mikell@gmail.com>
2022-07-02 18:07:42 +0000

    5.0.2 - FEAT: add country and state selects; state depends on country

--- notes/05-FormValidation.md
+++ notes/05-FormValidation.md
@@ -17,3 +17,31 @@ He provides the code. Let's try to understand what we're doing before we copy/pa
 -  Works so far
 
 **COMMIT: 5.0.1 - FEAT: create stub checkout component and wire into routes**
+
+Looking at what he pasted, I see
+
+-  `<select>` for country -- label, select, options (China, India, UK, US)
+-  `<input>` submit button (`className="btn btn-primary"`)
+-  Looks like some `<input>`s for address (city, more TBD)
+-  An `address` object with city and country (probably more)
+-  Nope, at the top of the form, he only has city and country in his empty address object
+   -  I may be more ambitious
+-  Handlers for change, blur (bound each input) and submit (submit button) that are "TODO"
+
+Given that, I'll draft my form.
+
+-  Define values for countries and country states
+-  Define address as ship to name, 2 address lines, city, state, postal code, country
+   -  Require country before enabling state (future)
+-  Add selects for state and country
+
+Let's get them working first. (Real world, I'd probably give some of these controls separate handlers, maybe even make them separate components.)
+
+-  `handleChange`
+   -  When the value changes, update state
+   -  If the changed value is country, reset state too
+      -  I can use `target.id` and check for `countryCode`
+
+It works
+
+**COMMIT: 5.0.2 - FEAT: add country and state selects; state depends on country**
--- shoe-store/src/components/Checkout.tsx
+++ shoe-store/src/components/Checkout.tsx
@@ -1,3 +1,129 @@
+import { BaseSyntheticEvent, useState } from 'react';
+
+// I want to filter the list of states by country
+const countryStates = [
+	{ countryCode: 'US', stateCode: 'CA', stateName: 'California' },
+	{ countryCode: 'US', stateCode: 'FL', stateName: 'Florida' },
+	{ countryCode: 'US', stateCode: 'NC', stateName: 'North Carolina' },
+	{ countryCode: 'CA', stateCode: 'ON', stateName: 'Ontario' },
+	{
+		countryCode: 'CA',
+		stateCode: 'NL',
+		stateName: 'Newfoundland and Labrador',
+	},
+	{ countryCode: 'CA', stateCode: 'BC', stateName: 'British Columbia' },
+	{ countryCode: 'JP', stateCode: 'Aichi', stateName: 'Aichi' },
+	{ countryCode: 'JP', stateCode: 'Saitama', stateName: 'Saitama' },
+	{ countryCode: 'JP', stateCode: 'Aomori', stateName: 'Aomori' },
+];
+
+const countries = [
+	{ countryCode: 'US', countryName: 'United States' },
+	{ countryCode: 'CA', countryName: 'Canada' },
+	{ countryCode: 'JP', countryName: 'Japan' },
+];
+interface IAddress {
+	shipToName: string;
+	addressLine1Text: string;
+	addressLine2Text: string;
+	cityName: string;
+	stateCode: string; // or any other second level division of a country
+	postalCode: string;
+	countryCode: string;
+}
+
+const emptyAddress: IAddress = {
+	shipToName: '',
+	addressLine1Text: '',
+	addressLine2Text: '',
+	cityName: '',
+	stateCode: '',
+	postalCode: '',
+	countryCode: '',
+};
+
 export function Checkout() {
-	return <div>Checkout</div>;
+	const [address, setAddress] = useState(emptyAddress);
+
+	function handleChange(e: BaseSyntheticEvent) {
+		e.preventDefault();
+
+		setAddress((oldAddress) => {
+			const newAddress: { [key: string]: any } = { ...oldAddress };
+			if (e.target.id === 'countryCode') {
+				newAddress.stateCode = '';
+			}
+			newAddress[e.target.id] = e.target.value;
+			return newAddress as IAddress;
+		});
+		console.log(e);
+		//TODO
+	}
+	function handleBlur(e: any) {
+		//TODO
+	}
+	function handleSubmit(e: any) {
+		//TODO
+	}
+
+	return (
+		<>
+			<div>
+				<label htmlFor="countryCode">Country</label>
+				<br />
+				<select
+					id="countryCode"
+					value={address.countryCode}
+					onBlur={handleBlur}
+					onChange={handleChange}
+				>
+					<option
+						key=""
+						value=""
+					>
+						Select Country
+					</option>
+					{countries.map((country) => (
+						<option
+							key={country.countryCode}
+							value={country.countryCode}
+						>
+							{country.countryName}
+						</option>
+					))}
+				</select>
+			</div>
+			<div>
+				<label htmlFor="stateCode">State/Province</label>
+				<br />
+				<select
+					id="stateCode"
+					value={address.stateCode}
+					onBlur={handleBlur}
+					onChange={handleChange}
+					disabled={address.countryCode === ''}
+				>
+					<option
+						key=""
+						value=""
+					>
+						Select State/Province
+					</option>
+					{countryStates
+						.filter(
+							(countryState) =>
+								countryState.countryCode === address.countryCode
+						)
+						.map((countryState) => (
+							<option
+								key={countryState.stateCode}
+								value={countryState.stateCode}
+							>
+								{countryState.stateName}
+							</option>
+						))}
+				</select>
+			</div>{' '}
+		</>
+	);
 }
